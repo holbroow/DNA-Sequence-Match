74a1fda676b9797853db90d46a941322f16ebe73
Will Holbrook <willholbrook10@gmail.com>
2023-12-11 11:24:32 +0000

    Task 3 gathers possible vars, now need to compare

--- task3.js
+++ task3.js
@@ -1,9 +1,10 @@
 // TASK 3 (TASK 1 AND 2 EXTENDED)
 
 "use strict";
-const testlib = require( './testlib.js' ); 		// SPECIFY THE USE OF THE TESTLIB FILE FOR FUNCTIONS
+const testlib = require( './testlib.js' );
 
-let possibleNucleotides = {                     // ALL POSSIBLE ACTUAL NUCLEOTIDES FOR EACH RECORDED CHARACTER
+
+let possibleNucleotides = {             // ALL POSSIBLE ACTUAL NUCLEOTIDES FOR EACH RECORDED CHARACTER
     'R' : ['G', 'A'],
     'Y' : ['T', 'C'],
     'K' : ['G', 'T'],
@@ -17,13 +18,13 @@ let possibleNucleotides = {                     // ALL POSSIBLE ACTUAL NUCLEOTID
     'N' : ['A', 'G', 'C', 'T']
 }
 let sequences = [];						// PATTERNS TO BE SEARCHED FOR
-let possiblePatterns = [];              // POSSIBLE PATTERNS FROM THE INPUT SEQUENCES, GIVEN POSSIBLE NUCLEOTIDES
 let patternFrequency = {};			    // THE FREQUENCY OF PATTERN APPEARANCE
-let prevLetter;						    // THE NEXT	LETTER TO BE CONSIDERED
 let currentLetter;					    // THE CURRENT LETTER BEING CONSIDERED
-let concatPattern;					    // THE CONCATENATED STRING TO COMPARE TO THE PATTERN
 let letterCount = 0;                    // NUMBER OF LETTERS CHECKED/ITERATED THROUGH
-let splitSequence = [];
+let largestSequenceSize = 0;            // THE NUMBER OF CHARACTERS IN THE LARGEST SEQUENCE (USED WHEN DECIDING A BUFFER SIZE)
+let buffer;                             // THE BUFFER THAT STORES THE LAST n CHARACTERS FROM THE DATA FILE
+let allVariations;                      // ALL VARIATIONS FOUND FROM USING POSSIBLE NUCLEOTIDES AND PROVIDED SEQUENCES
+let bufferString;                       // THE BUFFER AT ANY GIVEN POINT, CONVERTED TO A PLAIN STRING FOR COMPARISON
 
 
 // Function to generate variations for a given sequence
@@ -58,22 +59,30 @@ function generateVariations(sequence) {
 }
 
 
-
 testlib.on('ready', function(patterns) {
     // Assign the input patterns to the variable 'sequences'
     sequences = patterns;
     // Print the received sequences to the console
     console.log("Sequences:", sequences);
 
+    // DECIDE THE CHARACTER BUFFER SIZE FOR THE TEST (SIZE OF LARGEST SEQUENCE)
+    sequences.forEach(element => {
+        if (element.length > largestSequenceSize) {
+            largestSequenceSize = element.length;
+        }
+    });
+    console.log(largestSequenceSize);
+    buffer = new Array(largestSequenceSize);
+
     // Generate all variations for each sequence and flatten the resulting arrays into a single array
-    const allVariations = sequences.flatMap(sequence => generateVariations(sequence));
+    allVariations = sequences.flatMap(sequence => generateVariations(sequence));
     
     // PRINT ALL POSSIBLE PATTERNS
     console.log("Possible Patterns:", allVariations);
 
     // RUN TESTS
-    //// testlib.runTests();
-});
+    testlib.runTests();
+} );
 
 
 // THIS IS THE CODE THAT ACTS ON EACH PIECE OF DATA
@@ -82,20 +91,31 @@ testlib.on( 'data', function( data ) {
 	currentLetter = data;
 	console.log( "<<<", currentLetter ); // PRINT CURRENT LETTER READ
 	
-	concatPattern = prevLetter + currentLetter;
-	console.log("<<<<<<", concatPattern); // PRINT CONCATENATED STRING (PATTERN IN QUESTION)
+    if (buffer.length < largestSequenceSize) {
+        buffer.push(currentLetter);
+    } else {
+        buffer.shift();
+        buffer.push(currentLetter);
+    }
+    bufferString = buffer.join("");
+	console.log("<<<<<<", bufferString); // PRINT CONCATENATED STRING (PATTERN IN QUESTION)
 
 	// IF THE CONCATENATED STRING IS A PATTERN, UPDATE ITS VALUE IN THE TABLE
-	if (patternsToSearch.includes(concatPattern)) {
-		if (!patternFrequency[concatPattern]) {
-			patternFrequency[concatPattern] = 1;
-		} else {
-			patternFrequency[concatPattern] += 1;
-		}
-        testlib.foundMatch(concatPattern, letterCount);
-	}
+    allVariations.forEach(element => {
+        let varLength = element.length;
+        bufferString.slice(bufferString.length - varLength);
+        console.log(bufferString);
 
-	prevLetter = currentLetter;	// STORE THE CHARACTER AS A PREVIOUS CHARACTER FOR THE NEXT ITERATION
+        if (allVariations.includes(bufferString)) {
+            console.log("YES");
+            if (!patternFrequency[bufferString]) {
+                patternFrequency[bufferString] = 1;
+            } else {
+                patternFrequency[bufferString] += 1;
+            }
+            testlib.foundMatch(bufferString, letterCount);
+        }
+    });
 } );
 
 
